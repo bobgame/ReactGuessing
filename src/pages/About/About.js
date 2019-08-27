104434fd2163c30ab13ab09e91463232562787e2
Bob Liu <liulingbo@foxmail.com>
2019-08-27 17:49:35 +0800

    add some word to about page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -11,7 +11,8 @@ class About extends Component {
       <>
         <h2 className="text-center">关于</h2>
         <div className="mbh-2">
-          Create by Bob Liu (liulingbo@foxmail.com).
+          <p>Create by Bob Liu (liulingbo@foxmail.com).</p>
+          <p>Github: <a href="https://github.com/bobgame/ReactGuessing" target="_blank">https://github.com/bobgame/ReactGuessing</a></p>
         </div>
         <Link to="/"><Button block>返回首页</Button></Link>
       </>
