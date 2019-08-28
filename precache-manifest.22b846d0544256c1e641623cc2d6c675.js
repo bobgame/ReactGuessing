9672d1c9e11ec5d21c216744123e564a37819235
Bob Liu <liulingbo@foxmail.com>
2019-08-28 14:14:40 +0800

    Updates

---
+++ precache-manifest.22b846d0544256c1e641623cc2d6c675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dbdb66d909da4a346ddb0e3a9b265c",
+    "url": "/ReactGuessing/demo/index.html"
+  },
+  {
+    "revision": "b057d85f16084720b0bd",
+    "url": "/ReactGuessing/demo/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "381b3c408c8fe7340c02",
+    "url": "/ReactGuessing/demo/static/css/main.54b8c9ce.chunk.css"
+  },
+  {
+    "revision": "b057d85f16084720b0bd",
+    "url": "/ReactGuessing/demo/static/js/2.e5774703.chunk.js"
+  },
+  {
+    "revision": "381b3c408c8fe7340c02",
+    "url": "/ReactGuessing/demo/static/js/main.0ac1649b.chunk.js"
+  },
+  {
+    "revision": "425956447e57190d5e5f",
+    "url": "/ReactGuessing/demo/static/js/runtime~main.ae1f823b.js"
+  }
+]);
